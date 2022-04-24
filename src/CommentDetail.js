60267c59b90c292b0f2e236d1dab160a5d882245
xelva <xelvacodes@gmail.com>
2022-04-24 15:54:21 -0500

    done w project

---
+++ src/CommentDetail.js
@@ -0,0 +1,27 @@
+import React from 'react';
+
+
+const CommentDetail = props => {
+    return (
+        <div className="comment">
+                <a href="/" className="avatar">
+                    <img alt="avatar" src={props.profile}/>
+                </a>
+                <div className="content">
+                    <a href="/" className="author">
+                        {props.author}
+                    </a>
+                    <div className="metadata">
+                        <span className="date">
+                            {props.date}
+                            </span>
+                    </div>
+                    <div className="text">
+                        {props.comment}
+                    </div>
+                </div>
+        </div>
+    )
+}
+
+export default CommentDetail;
--- src/index.js
+++ src/index.js
@@ -1,28 +1,54 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import CommentDetail from './CommentDetail.js';
 import { faker } from '@faker-js/faker';
+import ApprovalCard from './ApprovalCard.js';
 const root = ReactDOM.createRoot(document.querySelector('#root'));
 
+//author name
+const auth1 = 'Jane';
+const auth2 = 'Bob';
+const auth3 = 'Jaaz';
 
-const profileAvatar = faker.image.avatar();
+//current time
+const date = new Date();
+const time = date.getHours() + ":" + date.getMinutes() + ":" + date.getSeconds();
+
+//comments
+const comment1 = 'hey there';
+const comment2 = 'whaaaaat this is so dope';
+const comment3 = 'yyyeeeeee!!!!';
+
+//author profile
+const profile1 = faker.image.avatar();
+const profile2 = faker.image.avatar();
+const profile3 = faker.image.avatar();
 
 const App = () => {
     return (
         <div className="ui container comments">
-            <div className="comment">
-                <a href="/" className="avatar">
-                    <img alt="avatar" src={profileAvatar}/>
-                </a>
-                <div className="content">
-                    <a href="/" className="author">
-                        Sam
-                    </a>
-                    <div className="metadata">
-                        <span className="date">Today at 6:00pm</span>
-                    </div>
-                    <div className="text">Nice blog post!</div>
-                </div>
-            </div>
+            <ApprovalCard>
+                <CommentDetail 
+                    author={auth1} 
+                    date={time} comment={comment1} 
+                    profile={profile1}
+                    />
+            </ApprovalCard>
+            <ApprovalCard>
+                <CommentDetail 
+                    author={auth2} 
+                    date={time} 
+                    comment={comment2} 
+                    profile={profile2}
+                    />
+            </ApprovalCard>
+            <ApprovalCard>
+                <CommentDetail 
+                    author={auth3} 
+                    date={time} 
+                    comment={comment3} 
+                    profile={profile3}/>
+            </ApprovalCard>
         </div>
     )
 }
